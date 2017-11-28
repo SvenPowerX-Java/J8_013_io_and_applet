6742e21ce0d2dc0e3cd56a8fd9f590e6a8ecca3f
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-28 15:54:40 +0100

    Чтение символов с консоли

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test {
+	public static void main(String[] args) throws IOException {
+//		BufferedInputStream bufferedInputStream = new BufferedInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+//		str = br.read();
+	}
+}
