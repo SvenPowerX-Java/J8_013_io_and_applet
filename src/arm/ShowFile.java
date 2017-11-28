cb403836c012fe9718ebdf22f11730cbf9230dde
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-28 20:05:51 +0100

    and :стр-372 АВтоматическое закрытие файла

---
+++ src/arm/ShowFile.java
@@ -0,0 +1,25 @@
+package arm;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ShowFile {
+	public static void main(String[] args) {
+		int i;
+		if (args.length != 1) {
+			System.out.println("Использование: ShowFile имя_файла");
+			return;
+		}
+		try (FileInputStream fin = new FileInputStream(args[0])) {
+			do {
+				i = fin.read();
+				if (i != -1) System.out.print((char) i);
+			} while (i != -1);
+		} catch (FileNotFoundException e) {
+			System.out.println("Файл не найден.");
+		} catch (IOException e) {
+			System.out.println("Произошла ошибка ввода-вывода");
+		}
+	}
+}
