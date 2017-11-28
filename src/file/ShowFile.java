b34c0aa9c14288cf1347a98c088e91627c66a2cb
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-28 17:54:10 +0100

    Чтение из файла ver_2

--- src/file/ShowFile.java
+++ src/file/ShowFile.java
@@ -1,8 +1,6 @@
 package file;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 /*
 	Отображение содержимого текстового файла.
@@ -10,12 +8,18 @@ import java.io.IOException;
 	имя файла, который требуется посмотреть.
 	Например, чтобы посмотреть файл test.txt,
 	введите в командной строке следующую команду:
+	
 	java ShowFile test.txt
+	
+	В этом варианте программы код, открывающий и получающий
+	доступ к файлу, заключён в один блок оператора try.
+	файл закрывается в блоке оператора finally
  */
 public class ShowFile {
 	public static void main(String[] args) {
 		int i;
-		FileInputStream fin;
+		FileInputStream fin = null;
+		
 		//проверим что имя файла указано
 		if (args.length != 1) {
 			System.out.println("Использование: ShowFile имя_файла");
@@ -23,27 +27,22 @@ public class ShowFile {
 		}
 		try {
 			fin = new FileInputStream(args[0]);
-		} catch (FileNotFoundException e) {
-			System.out.println("Невозможно открыть файл");
-			return;
-		}
-		//  Теперь файл открыт и готов к чтению.
-		//  Далее из него читаются символы до тех пор,
-		//  пока не встретится признак конца файла
-		
-		try {
 			do {
 				i = fin.read();
 				if (i != -1) System.out.print((char) i);
 			} while (i != -1);
+		} catch (FileNotFoundException e) {
+			System.out.println("Файл ненайден.");
 		} catch (IOException e) {
 			System.out.println("Ошибка чтения из файла");
+		}finally {
+			//закрыть файл
+			try {
+				if(fin !=null)fin.close();
+			} catch (IOException e) {
+				System.out.println("Ошибка закрытия файла");
+			}
 		}
-		//закрыть файл
-		try {
-			fin.close();
-		} catch (IOException e) {
-			System.out.println("Ошибка закрытия файла");
-		}
+		
 	}
 }
