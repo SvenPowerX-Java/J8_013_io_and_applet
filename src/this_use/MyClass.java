567a4bbba55b1ead74216585a74c99007cc2aec2
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-30 14:32:40 +0100

    Использование This version 2

--- src/this_use/MyClass.java
+++ src/this_use/MyClass.java
@@ -4,17 +4,16 @@ public class MyClass {
 	int a;
 	int b;
 	
-	public MyClass() {
-		a = 0;
-		b = 0;
+	public MyClass(int i, int j) {
+		a = i;
+		b = j;
 	}
 	
 	public MyClass(int a) {
-		this.a = a;
+		this(a, a);
 	}
 	
-	public MyClass(int a, int b) {
-		this.a = a;
-		this.b = b;
+	public MyClass() {
+		this(0);
 	}
 }
