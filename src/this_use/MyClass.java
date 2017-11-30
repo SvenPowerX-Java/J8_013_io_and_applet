70746f5805bef0dfdcc0443909a2b48cd4e997da
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-30 14:18:53 +0100

    Использование This

---
+++ src/this_use/MyClass.java
@@ -0,0 +1,20 @@
+package this_use;
+
+public class MyClass {
+	int a;
+	int b;
+	
+	public MyClass() {
+		a = 0;
+		b = 0;
+	}
+	
+	public MyClass(int a) {
+		this.a = a;
+	}
+	
+	public MyClass(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+}
