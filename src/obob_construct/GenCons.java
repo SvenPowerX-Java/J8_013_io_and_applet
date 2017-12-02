f82e5a8b27a067ac20b059da67c003e64fc80468
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-02 20:27:01 +0100

    стр - 418 Обобщённые конструкторы

---
+++ src/obob_construct/GenCons.java
@@ -0,0 +1,12 @@
+package obob_construct;
+
+public class GenCons {
+	private double val;
+	
+	<T extends Number> GenCons(T arg) {
+		val = arg.doubleValue();
+	}
+	void showval() {
+		System.out.println("val = " + val);
+	}
+}
---
+++ src/obob_construct/GenConsDemo.java
@@ -0,0 +1,10 @@
+package obob_construct;
+
+public class GenConsDemo {
+	public static void main(String[] args) {
+		GenCons test = new GenCons(100);
+		GenCons test2 = new GenCons(123.5F);
+		test.showval();
+		test2.showval();
+	}
+}
