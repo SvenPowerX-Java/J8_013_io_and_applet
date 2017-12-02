aeb08a91e6d82b34b324d32828fca65b5ebf8771
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-02 19:54:19 +0100

    Ограниченные метасимвольные аргументы

---
+++ src/ogranichenni_meta_simbol/ThreeD.java
@@ -0,0 +1,10 @@
+package ogranichenni_meta_simbol;
+
+public class ThreeD extends TwoD {
+	int z;
+	
+	public ThreeD(int x, int y, int z) {
+		super(x, y);
+		this.z = z;
+	}
+}
---
+++ src/ogranichenni_meta_simbol/TwoD.java
@@ -0,0 +1,12 @@
+package ogranichenni_meta_simbol;
+
+//Двумерные координаты
+
+public class TwoD {
+	int x, y;
+	
+	public TwoD(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
